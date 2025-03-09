8dca29bcfe9bd219a3c512446eb3812dd32eaa11
Fabio <fabio.bocconi@gmail.com>
2025-03-09 11:37:13 +0100

    update vista

--- esercizi/esempi_django/corsopin/polls/views.py
+++ esercizi/esempi_django/corsopin/polls/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from polls.models import Question, Choice
+from pprint import pprint
 
 def index(request):
+    # queryset = Question.objects.all()
+    # pprint(queryset)
+    # pprint(queryset[0].choices.all())
     return HttpResponse("Hello, world. You're at the polls index.")
