fb9fa51ed5cfa9c0c17970444cc76368a8ff20b1
Fabio <fabio.bocconi@gmail.com>
2025-03-09 14:27:29 +0100

    aggiunte altre viste a corredo degli esempi

--- esercizi/esempi_django/corsopin/polls/views.py
+++ esercizi/esempi_django/corsopin/polls/views.py
@@ -1,10 +1,34 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
+
 from polls.models import Question, Choice
 from pprint import pprint
 
 def index(request):
-    # queryset = Question.objects.all()
+    queryset = Question.objects.all()
     # pprint(queryset)
     # pprint(queryset[0].choices.all())
-    return HttpResponse("Hello, world. You're at the polls index.")
+    # queryset = Question.objects.filter(question_text="test")
+    latest_question_list = Question.objects.order_by("-pub_date")[:5]
+    template = loader.get_template("polls/index.html")
+    print(queryset)
+
+    context = {
+        "latest_question_list": queryset,
+    }
+    return HttpResponse(template.render(context, request))
+
+
+def detail(request, question_id):
+    print(question_id)
+    return HttpResponse("You're looking at question %s." % question_id)
+
+
+def results(request, question_id):
+    response = "You're looking at the results of question %s."
+    return HttpResponse(response % question_id)
+
+
+def vote(request, question_id):
+    return HttpResponse("You're voting on question %s." % question_id)
