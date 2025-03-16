f1312bd92982c29fca86cc159c0f566b2c516631
Fabio <fabio.bocconi@gmail.com>
2025-03-16 12:22:07 +0100

    bump struttura del blog

--- esercizi/esempi_django/corsopin/blogs/views.py
+++ esercizi/esempi_django/corsopin/blogs/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from pprint import pprint
+from django.http import HttpResponse, HttpResponse
 
 # Create your views here.
+def index(request):
+    return HttpResponse("index")
